6e47f5a9131a64f845d850104beaa4817a344b9b
Lindsay Naki <lindsnakamura@gmail.com>
2022-02-04 21:02:41 -0800

    added patch method to posts and get method to tagname/posts

--- api/posts.js
+++ api/posts.js
@@ -51,7 +51,38 @@ postsRouter.post('/', requireUser, async (req, res, next) => {
 // PATCH /api/posts/:postId
 postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
   const { postId } = req.params;
-  
-})
+  const { title, content, tags } = req.body; 
+
+  const updateFields = {} ;
+
+  if (tags && tags.length > 0) {
+    //adding tags as an array to object updateFields as key and values
+    updateFields.tags = tags.trim().split(/\s+/);
+  }
+
+  if (title) {
+    updateFields.title = title;
+  }
+
+  if (content) {
+    updateFields.content = content; 
+  }
+
+  try {
+    const originalPost = await getPostById(postId);
+
+    if (originalPost.author.id === req.user.id) {
+      const updatedPost = await updatePost(postId, updateFields);
+      res.send({post: updatedPost})
+    } else {
+      next({
+        name: 'UnauthorizedUserError',
+        message: 'You cannot update a post that is not yours'
+      })
+    }
+  } catch ({ name, message }) {
+    next({ name, message }); 
+  }
+});
 
 module.exports = postsRouter;
